dfcb25b68c2bb7c89ca2dbdb228694d9e09f67aa
bmathiasen <soborg@diku.dk>
2010-05-13 15:41:12 +0200

    cleanup of methods, renaming of methods, beautification

--- datastructure.py
+++ datastructure.py
@@ -2,35 +2,37 @@ from abstract import abstract
 
 class GraphDatastructure():
 
-    def vertexexists(self, node_id): abstract()
-
-    def edgeexists(self, edge_id): abstract()
+    def addVertex(self, vertex_id): abstract()
     
-    def removeedge(self, (node_id_1, node_id_2), edge_id=None): abstract()
+    def addEdge(self, (vertex_id_1, vertex_id_2), weight=0): abstract()
 
-    def removeedges(self, edge_list): abstract()
+    def vertexExists(self, node_id): abstract()
 
-    def getvertices(self): abstract()
+    def edgeExists(self, edge_id): abstract()
     
-    def getedges(self):  abstract()
+    def removeEdge(self, (node_id_1, node_id_2), edge_id=None): abstract()
+
+    def removeEdges(self, edge_list): abstract()
+
+    def getVertices(self): abstract()
+    
+    def getEdges(self):  abstract()
     
     def degreelist(self):  abstract()
     
-    def getadjedges(self, node_id): abstract()
+    def getAdjEdges(self, node_id): abstract()
 
-    def getadjvertices(self, node_id): abstract()
+    def getAdjVertices(self, node_id): abstract()
         
-    def getvertex(self, node_id):  abstract()
+    def getVertex(self, node_id):  abstract()
     
-    def __str__(self): abstract()
-
     # the following functions are following a strict naming scheme across
     # our datastructures
-    def visualisegraph(self, figNum=1, markEdges=[], markNodes=[]): abstract()
+    def visualise(self, figNum=1, markEdges=[], markNodes=[]): abstract()
 
-    def clearvisualisation(self): abstract()
+    def clearVisualisation(self): abstract()
         
-    def generaterandomstructure(self, n): abstract()
+    def generateRandomGraph(self, n): abstract()
         
     def clear(self):  abstract()
 
